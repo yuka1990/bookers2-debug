81d2cf9276de5598d6555daff0d9e3a2ce5636e9
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-23 08:32:57 +0000

    [update]comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "home/about"=>"homes#about"
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-  resources :book_comments, only:[:create]
+  resources :book_comments, only:[:create,:destroy]
   resource :favorites, only: [:create,:destroy]
   end
   resources :users, only: [:index,:show,:edit,:update]
