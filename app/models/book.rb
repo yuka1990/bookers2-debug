eed03d8472177d9f7a638861c14d6c25327974bc
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-24 09:41:15 +0000

    [revise]課題5

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,6 +14,10 @@ class Book < ApplicationRecord
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('title LIKE?', content + '%')
+    elsif method == 'backward'
+      Book.where('title LIKE?', '%' + content)
     else
       Book.where('title LIKE?' , '%' + content + '%')
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,10 @@ end
 def self.search_for(content, method)
   if method == 'perfect'
     User.where(name: content)
+  elsif method == 'forward'
+    User.where('name LIKE?', content + '%')
+  elsif method == 'backward'
+    User.where('name LIKE?','%'+content)
   else 
     User.where('name LIKE ?', '%' + content + '%')
   end
