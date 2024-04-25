27dcd11a4083fa2850c406f26e8b5831935f5770
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-25 07:04:50 +0000

    [revise]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,14 +1,13 @@
 class BookCommentsController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     @book_comment =current_user.book_comments.new(book_comment_params)
-    @book_comment.book_id=@book.id
+    @book_comment.book_id=book.id
     @book_comment.save
   end
   
   def destroy
-    @book = Book.find(params[:book_id])
-    @book_comment = BookComment.find(params[:id]).destroy
+    @book_comment = BookComment.find(params[:id])
     @book_comment.destroy
   end
   
